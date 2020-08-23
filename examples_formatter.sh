c329f57c3039d94885043db16d2f46d6fe7a5305
per1234 <accounts@perglass.com>
2020-08-23 07:04:19 -0700

    Add all supported sketch filename extensions to examples_formatter.sh

--- examples_formatter.sh
+++ examples_formatter.sh
@@ -1,2 +1,2 @@
 # you need to have astyle installed before running this
-find examples -name '*.ino' -exec astyle --options=examples_formatter.conf {} \;
+find examples -regextype posix-extended -regex '.*\.((ino)|(h)|(cpp)|(c))$' -and -type f -exec astyle --options=examples_formatter.conf {} \;
