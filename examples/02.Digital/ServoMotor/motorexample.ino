f06a970d784eb52a57d3acdb20ae3cb5ec9e7ee4
Paula <terceranexus6@users.noreply.github.com>
2021-10-27 14:18:15 +0200

    example for servo motor
    Code I used for teaching robotics, in steps of 90 degrees forward and back, based on the code of Alex Glow https://create.arduino.cc/projecthub/glowascii/servo-arduino-basics-cb9266

---
+++ examples/02.Digital/ServoMotor/motorexample.ino
@@ -0,0 +1,34 @@
+/*
+ The original code is based in Alex Glow 
+ https://create.arduino.cc/projecthub/glowascii/servo-arduino-basics-cb9266
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on digital pin 9 to the servo object
+}
+
+void loop() {
+  for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 90; pos >= 180; pos += 1) { // goes from 90 degrees to 180 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 90; pos -= 1) { // goes from 180 degrees to 90 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 90; pos >= 0; pos -= 1) { // goes from 90 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
