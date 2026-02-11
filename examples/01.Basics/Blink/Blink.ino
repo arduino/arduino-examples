6a2482f5a33824b00d1006fbd65e5c0555a0da56
per1234 <accounts@perglass.com>
2026-02-10 22:53:04 -0800

    Adjust comments to avoid assumption of on-board LED circuit configuration
    The "Blink" example controls the on-board LED. Some boards use an active high circuit configuration for this LED, while
    other boards use an active low configuration.
    
    Previously the comments on the `digitalWrite` calls in the example assumed an active high circuit in their description
    of the effect. This meant the comments were incorrect when used with a board that has an active low circuit. This
    misinformation is especially harmful since it is common for beginners to have the impression that HIGH always means "on"
    and LOW always means "off".
    
    Those comments are hereby reworded to avoid describing a specific LED state, so that they will be equally correct for
    all boards.
    
    The alternative approach would be to provide an explanation of active high and low circuit configurations and the
    resulting correlation between pin and LED states. However, such an explanation would necessarily be far more verbose
    than is appropriate in this context, and would make the example less approachable to the target beginner user.

--- examples/01.Basics/Blink/Blink.ino
+++ examples/01.Basics/Blink/Blink.ino
@@ -30,8 +30,8 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);  // change state of the LED by setting the pin to the HIGH voltage level
   delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);   // change state of the LED by setting the pin to the LOW voltage level
   delay(1000);                      // wait for a second
 }
